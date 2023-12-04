d3fc067242cd9e40b13936031c75d9ff36cbf94a
quinterodiego <d86webs@gmail.com>
2023-12-04 15:27:47 -0300

    segunda pre entrega

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -1,5 +1,17 @@
 import * as service from '../services/cart.services.js'
 
+export const create = async (req, res) => {
+  const newCart = await service.create()
+  if(newCart) {
+    res.status(200).send({
+      "status": "success",
+      "message": newCart
+    })
+  } else {
+    res.status(404).send({ error: 'Error al crear el carrito'})
+  }
+}
+
 export const getById = async (req, res) => {
   const id = req.params.pid
   const cart = await service.getById(id)
@@ -10,18 +22,6 @@ export const getById = async (req, res) => {
     })
   } else {
     res.status(404).send({ error: 'Carrito no encontrado'})
-  }
-}
-
-export const create = async (req, res) => {
-  const newCart = await service.create()
-  if(newCart) {
-    res.status(200).send({
-      "status": "success",
-      "message": newCart
-    })
-  } else {
-    res.status(404).send({ error: 'Error al crear el carrito'})
   }
 }
 
@@ -38,4 +38,48 @@ export const addProductByIdInCart = async (req, res) => {
   } else {
     res.status(404).send({ error: 'Error al actualizar el carrito'})
   }
+}
+
+export const deleteProduct = async (req, res) => {
+  const idCart = req.params.cid
+  const idProduct = req.params.pid
+  const resp = await cartService.deleteProduct(idCart, idProduct)
+  
+  res.status(201).send({
+      "status": "success",
+      "message": resp
+  })
+}
+
+export const deleteProducts = async (req, res) => {
+  const idCart = req.params.cid
+  const products = req.body.products
+  const resp = await cartService.deleteProducts(idCart, products)
+  
+  res.status(201).send({
+      "status": "success",
+      "message": resp
+  })
+}
+
+export const updateProducts = async (req, res) => {
+  const idCart = req.params.cid
+  const idProduct = req.params.pid
+  const quantity = req.body.quantity
+  const resp = await cartService.updateProducts(idCart, idProduct, quantity)
+  
+  res.status(201).send({
+      "status": "success",
+      "message": resp
+  })
+}
+
+export const updateQuantity = async (req, res) => {
+  const idCart = req.params.cid
+  const resp = await cartService.updateQuantity(idCart)
+  
+  res.status(201).send({
+      "status": "success",
+      "message": resp
+  })
 }
--- src/routers/carts.router.js
+++ src/routers/carts.router.js
@@ -8,5 +8,10 @@ cartsRouter.post('/', controller.create)
 cartsRouter.get('/:cid', controller.getById)
 
 cartsRouter.put('/:cid/product/:pid', controller.addProductByIdInCart)
+cartsRouter.put('/:cid', controller.deleteProducts)
+cartsRouter.put('/:cid/products/:pid', controller.updateProducts)
+
+cartsRouter.delete('/:cid/products/:pid', controller.deleteProduct)
+cartsRouter.delete('/:cid', controller.updateQuantity)
 
 export default cartsRouter
