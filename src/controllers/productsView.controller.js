6c9122b84f86716baadc7c5845edffcf3cf50150
quinterodiego <d86webs@gmail.com>
2023-12-14 09:10:16 -0300

    desafio clase21

--- src/controllers/productsView.controller.js
+++ src/controllers/productsView.controller.js
@@ -3,11 +3,12 @@ import { getAll } from '../services/product.services.js'
 export const getAllProducts =  async (req, res) => {
   const { limit, page, sort, category, stock } = req.query
   const products = await getAll(limit, page, sort, category, stock)
+  console.log(req.session)
   const userData = {
-    firstname: req.session.user.firstname,
-    lastname: req.session.user.lastname,
-    email: req.session.user.email,
-    role: req.session.user.role
+    firstname: req.session.firstname,
+    lastname: req.session.lastname,
+    email: req.session.email,
+    role: req.session.role
   }
   console.log(req.session)
   products.userData = userData
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -8,7 +8,7 @@ export const authRouter = express.Router()
 
 authRouter.get('/', userController.loginView)
 
-authRouter.post('/login', passport.authenticate('login'), userController.login)
+authRouter.post('/login', passport.authenticate('login', { scope: ['user:email'] }), userController.login)
 
 authRouter.get('/register', userController.registerView)
 
@@ -21,6 +21,10 @@ authRouter.get('/github', passport.authenticate('github', { scope: ['user:email'
 authRouter.get('/githubcallback', passport.authenticate('github', { failureRedirect: '/faillogin' }), (req, res) => {
   console.log('req.user => ', req.user)
   req.session.user = req.user
+  req.session.firstname = req.user.firstname
+  req.session.lastname = req.user.lastname
+  req.session.email = req.user.email
+  req.session.role = req.user.role
   return res.redirect('/products')
 })
 
