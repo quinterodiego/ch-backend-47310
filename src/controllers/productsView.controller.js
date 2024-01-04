8f05fab9ededf300d2087915db23ca0a1e124111
quinterodiego <d86webs@gmail.com>
2024-01-04 17:13:51 -0300

    segunda practica integradora

--- src/controllers/productsView.controller.js
+++ src/controllers/productsView.controller.js
@@ -4,10 +4,8 @@ export const getAllProducts =  async (req, res) => {
   const { limit, page, sort, category, stock } = req.query
   const products = await getAll(limit, page, sort, category, stock)
   const userData = {
-    firstname: req.session.firstname,
-    lastname: req.session.lastname,
-    email: req.session.email,
-    role: req.session.role
+    email: req.user.email,
+    role: req.user.role
   }
   products.userData = userData
   res.status(200).render('products', products)
